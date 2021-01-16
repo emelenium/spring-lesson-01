ed707022206004c7edd3b8d7ce9565ae80963d6e
emelenium <emelenium@gmail.com>
2021-01-16 21:45:26 +0500

    Initial commit

---
+++ src/test/java/ru/education/lessons/springlesson01/SpringLesson01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.education.lessons.springlesson01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLesson01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
