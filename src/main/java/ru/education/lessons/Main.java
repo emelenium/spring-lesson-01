9355033f71315e84e706a9a8becf51096d1d6267
emelenium <emelenium@gmail.com>
2021-01-21 10:50:21 +0500

    Spring course first changes in example

--- src/main/java/ru/education/lessons/Main.java
+++ src/main/java/ru/education/lessons/Main.java
@@ -1,15 +1,21 @@
 package ru.education.lessons;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.education.lessons.domain.Person;
+import ru.education.lessons.service.PersonService;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-		Person ivan = new Person("Ivan",27);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+		//		SpringApplication.run(Main.class, args);
+		Person ivan = context.getBean(Person.class);
+	//	Person ivan = service.getByName("Ivan");
+
+//		Person ivan = new Person("Ivan",27);
 		System.out.println("name: "+ ivan.getName()+ " age "+ ivan.getAge());
 	}
 
